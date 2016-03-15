784c71d57a2f2443579529c0bc55d71e667a0af6
David Barnes <barnesdavidj@gmail.com>
2016-03-14 20:11:09 -0400

    Add Node class. Add Linked List Class with Add method. Add sample use'

---
+++ cis237inclass4/MyLinkedList.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cis237inclass4
+{
+    class MyLinkedList
+    {
+        //Node that will 'point' to the current node we are looking at
+        public Node Current
+        {
+            get;
+            set;
+        }
+
+        //Node that will 'point' to the beginning of the linked list
+        public Node Head
+        {
+            get;
+            set;
+        }
+
+        //Node that will 'point' to the last node in the linked list
+        public Node Tail
+        {
+            get;
+            set;
+        }
+
+        //Constructor. Just initalize the properties to null
+        public MyLinkedList()
+        {
+            Head = null;
+            Tail = null;
+            Current = null;
+        }
+
+        //This will be the add method to add a new node to the linked list
+        public void Add(string content)
+        {
+            //Make a new node
+            Node node = new Node();
+
+            //Set the data for the node to the content that was passed in.
+            node.Data = content;
+
+            //If Head is null, that means that there are no nodes in the linked list.
+            //We are about to add the first node.
+            if (Head == null)
+            {
+                Head = node;
+                //This next line got moved below the if/else, so it is no longer needed
+                //Tail = node;
+            }
+            //This is the case where there is already at least 1 node in the linked list. Maybe many.
+            else
+            {
+                //Take the Tail Node, which is the last one in the list, and set it's Next property
+                //which was null, to the new node we just created.
+                Tail.Next = node;
+
+                //This is a valid replacement for Tail = node;, but might be harder to understand.
+                //Tail = Tail.Next;
+                //This next line got moved below the if/else
+                //Tail = node;
+            }
+            //This was duplicated in both the if and the else, so we moved it down here since it must be
+            //done for both of them.
+            Tail = node;
+        }
+
+
+    }
+}
--- cis237inclass4/Program.cs
+++ cis237inclass4/Program.cs
@@ -10,6 +10,23 @@ namespace cis237inclass4
     {
         static void Main(string[] args)
         {
+            //Make a new linked list
+            MyLinkedList myLinkedList = new MyLinkedList();
+
+            //Add a few things to the linked list
+            myLinkedList.Add("first");
+            myLinkedList.Add("second");
+            myLinkedList.Add("third");
+            myLinkedList.Add("fourth");
+
+            //Loop through with this differently looking for loop to output.
+            //In here, the first part is initialization: Setting x to the Head
+            //the second part is the test: if x != null, keep going
+            //The last part is: Set the current x to x's next pointer. (The next in the list)
+            for (Node x = myLinkedList.Head; x != null; x = x.Next)
+            {
+                Console.WriteLine(x.Data);
+            }
         }
     }
 }
